4a5191a98d216f1e0560931a1a27ccd54da5428e
horiaomar25 <horiaomar6@gmail.com>
2024-02-16 16:56:47 +0000

    added: styling for login pages

--- edutask/app/page.js
+++ edutask/app/page.js
@@ -1,9 +1,17 @@
-
+"use client"
 
 
 export default function Home() {
   return (
     <>
+    <section className="border-2 border-black m-10 w-11/12 p-10 flex justify-center items-center ">
+      {/* This div is the container that is holding the input and the button*/}
+      <div className="bg-white border-2 border-black flex flex-col justify-center items-center rounded-lg p-10 m-10 w-11/12 md:w-11/12 lg:w-11/12 sm:w-full  ">
+         <p>Hello World</p>
+       
+      </div>
+
+    </section>
   
     </>
   );
