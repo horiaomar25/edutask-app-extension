4db4d99e11eec7513beba1b49ac910ad2069f5a2
horiaomar25 <horiaomar6@gmail.com>
2024-02-16 17:10:35 +0000

    continued styling for login

--- edutask/app/page.js
+++ edutask/app/page.js
@@ -4,9 +4,9 @@
 export default function Home() {
   return (
     <>
-    <section className="border-2 border-black m-10 w-11/12 p-10 flex justify-center items-center ">
+    <section className="flex flex-col justify-center items-center h-screen ">
       {/* This div is the container that is holding the input and the button*/}
-      <div className="bg-white border-2 border-black flex flex-col justify-center items-center rounded-lg p-10 m-10 w-11/12 md:w-11/12 lg:w-11/12 sm:w-full  ">
+      <div className=" border-2 border-black flex flex-col justify-center items-center rounded-lg p-10 m-10 w-11/12 .h-2/5 md:w-1/2 lg:w-1/2 sm:w-1/2">
          <p>Hello World</p>
        
       </div>
