2cf98221f1923f790699aa7a990efa84b0b867f8
horiaomar25 <horiaomar6@gmail.com>
2024-03-05 20:37:13 +0000

    added: MiniTaskCard data added. Styling to be continued

--- edutask/app/dashboard/page.js
+++ edutask/app/dashboard/page.js
@@ -2,13 +2,16 @@
 import DashboardLayout from './dashboardlayout' // Import DashboardLayout component
 import TaskCounter from '@/components/TaskCounter'
 import MiniTaskCards from '@/components/MiniTaskCards'
+import TaskData from '@/data/TaskData'
 
 
 const Dashboard = () => {
+  const { tasks } = TaskData();
+   
   return (
     <DashboardLayout>
     <TaskCounter/>
-    <MiniTaskCards/>
+    <MiniTaskCards tasks={tasks}/>
     </DashboardLayout>
   )
 
--- edutask/components/MiniTaskCards.jsx
+++ edutask/components/MiniTaskCards.jsx
@@ -1,30 +1,21 @@
 import React from 'react';
-import TaskData from '@/data/TaskData';
 
-const MiniTaskCards = () => {
-  const { tasks } = TaskData();
-
-  // Convert tasksIterable to an array using Array.from()
-  const tasksArray = Array.from(tasks);
-
-  // Selecting the last three tasks
-  const lastThreeTasks = tasksArray.slice(-3);
+const MiniTaskCards = ({ tasks }) => {
+  const dailyTasks = tasks.filter((task) => task.task_type === "Daily").slice(0, 3);
 
   return (
-    <>
-      <section className='flex justify-center items-center flex-row'>
-        {lastThreeTasks.map((task, index) => (
-          <div key={index} className="border border-black w-96 h-60 text-center rounded-md p-20 bg-white mt-10 ml-14">
-            <hr className="w-full"/>
-            {/* Render chip for task type */}
-            <div className="chip">{task.type === 'weekly' ? 'Weekly' : 'Daily'}</div>
-            {/* Render other task details */}
-            <p>{task.name}</p>
-            {/* Add more task details as needed */}
-          </div>
-        ))}
-      </section>
-    </>
+    <section className='flex justify-center items-center flex-row'>
+      {dailyTasks.map((task, index) => (
+        <div key={index} className="border border-black w-96 h-60  rounded-md p-4 bg-white  ml-14">
+          <p className='text-2xl '>{task.task_name}</p>
+          <hr className="w-full bg-black"/>
+          {/* Task Description */}
+          <p className='mt-auto'>{task.task_type}</p>
+          {/* Task Type */}
+         
+        </div>
+      ))}
+    </section>
   );
 }
 
