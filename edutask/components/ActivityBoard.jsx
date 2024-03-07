98985462f1d5860d9fbbdc1ca74914a0b75d7832
horiaomar25 <horiaomar6@gmail.com>
2024-03-07 14:24:38 +0000

    Added: idea board logo/button added

--- edutask/components/ActivityBoard.jsx
+++ edutask/components/ActivityBoard.jsx
@@ -4,6 +4,7 @@ const ActivityBoard = () => {
   return (
     <section className='flex justify-center items-center h-full'>
       <button className='bg-purple p-2 border-none w-60 rounded-md text-white'>Add Idea</button>
+      
     </section>
   )
 }
