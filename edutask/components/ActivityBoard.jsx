8db7e6cb0f0804c023d2d6a3bfd41cb4720f21e7
horiaomar25 <horiaomar6@gmail.com>
2024-03-17 15:21:20 +0000

    added card for idea board

--- edutask/components/ActivityBoard.jsx
+++ edutask/components/ActivityBoard.jsx
@@ -3,9 +3,12 @@ import BoardCard from "./BoardCard";
 const ActivityBoard = () => {
   return (
     <section className='flex justify-start items-start flex-row h-full m-8 p-8'>
+      <div className="flex flex-row m-8">
       <button className='bg-purple p-20 border-none w-40 rounded-md text-white text-center flex items-center justify-center'>Add Idea</button>
+      <div className="ml-8">
   <BoardCard/>
-      
+  </div>
+      </div>
     </section>
   )
 }
